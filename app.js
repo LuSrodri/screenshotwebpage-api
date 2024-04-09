b40ebd441618d4442e7a1af8b35b9c0f53b8c0cf
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2024-04-09 19:55:16 -0300

    Update api

--- app.js
+++ app.js
@@ -8,38 +8,41 @@ const port = process.env.PORT || 3000;
 const { chromium } = require('playwright');
 const { v4: uuidv4 } = require('uuid');
 
-app.get('/capture/:encodedURL', async (req, res) => {
-    const urlToCapture = req.params.encodedURL;
+app.get('/capture', async (req, res) => {
+    const urlToCapture = req.query.url;
     const fullPage = ((req.query.fullpage && req.query.fullpage.toLowerCase() === 'true'));
     const download = ((req.query.download && req.query.download.toLowerCase() === 'true'));
     const id = uuidv4();
     const browser = await chromium.launch();
     const context = await browser.newContext();
     const page = await context.newPage();
-    
+
     try {
         await page.goto(urlToCapture);
-        
+
         // Scroll to the bottom of the page
         await page.evaluate(() => {
-            window.scrollTo(0, document.body.scrollHeight);
+            let scrollHeight = document.body.scrollHeight;
+            do {
+                scrollHeight = document.body.scrollHeight;
+                window.scrollTo(0, document.body.scrollHeight);
+            } while (scrollHeight < document.body.scrollHeight);
         });
 
-        // Scroll back to the top of the page
+        // Scroll to the top of the page
         await page.evaluate(() => {
             window.scrollTo(0, 0);
         });
-        
+
         await page.screenshot({ path: (id + '.png'), fullPage });
 
-        if (download) res.download(`${__dirname}/${id}.png`, 'screenshotwebpage_com.png', {maxAge: (7*24*60*60*1000)});
-        else res.sendFile(`${__dirname}/${id}.png`, {maxAge: (7*24*60*60*1000)});
+        if (download) res.download(`${__dirname}/${id}.png`, 'screenshotwebpage_com.png', { maxAge: (7 * 24 * 60 * 60 * 1000) });
+        else res.sendFile(`${__dirname}/${id}.png`, { maxAge: (7 * 24 * 60 * 60 * 1000) });
     }
     catch (e) {
-        console.log(e);
         res.statusCode = 404;
 
-        res.sendFile(`${__dirname}/pageNotFound.png`, {maxAge: (30*24*60*60*1000)});
+        res.sendFile(`${__dirname}/pageNotFound.png`, { maxAge: (30 * 24 * 60 * 60 * 1000) });
     }
     await browser.close();
     return;
